f870eef5d237ae373bee4e3eb82b770abd190321
neeraj001122 <112713817+neeraj001122@users.noreply.github.com>
2022-11-07 18:50:38 +0530

    Task8

--- src/App.js
+++ src/App.js
@@ -2,26 +2,53 @@ import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import Loading from "./components/Loading";
 
+var arr = [];
 function App() {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null)
   async function fetchMovieHandler  () {
-      const res = await fetch("https://swapi.dev/api/films/")
+      setLoading(true)
+      setError(null)
+      try {
+        const res = await fetch("https://swapi.dev/api/film")
+        if(!res.ok)
+        {
+          throw new Error('Something went wrong... retrying')
+        }
       const data = await res.json();
        setMovies(data.results);
-      }
-
-
+      } catch (errror) {
+      setLoading(false)
+      const id = setInterval(() => {
+        fetchMovieHandler();
+      }, 5000)
+      arr.push(id)
+    }
+  }
+  const stopHandler = () => {
+    for(let id of arr)
+    {
+      console.log(id)
+      clearInterval(id)
+    }
+  }; 
+  console.log(arr)
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+      <button onClick={stopHandler}>Stop Retrying</button>
+       {!loading && <MoviesList movies={movies} />}
+       {!loading && error && <p>{error}</p>}
+       {loading && <Loading />}
       </section>
     </React.Fragment>
   );
-};
+}
 
 export default App;
---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <img
+      src="https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!bw700"
+      alt="Loading"
+    ></img>
+  );
+};
+
+export default Loading
